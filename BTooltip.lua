a64aacd335ab81428870a582fed573303c047453
Ethan Centaurai <ethancentaurai@outlook.com>
2010-11-06 10:31:17 +0000

    Create the tooltip in Lua instead of XML.

---
+++ BTooltip.lua
@@ -0,0 +1,5 @@
+
+VisorTooltip = CreateFrame("GameTooltip", "VisorTooltip", UIParent, "GameTooltipTemplate")
+VisorTooltip:Hide()
+VisorTooltip:SetFrameStrata("TOOLTIP")
+VisorTooltip:SetOwner(UIParent, "ANCHOR_NONE")
